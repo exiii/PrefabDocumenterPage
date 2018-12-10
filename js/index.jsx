a69e553736563b98056b3f0c633035a635ea4f00
pf35301 <pf35301@gmail.com>
2018-12-10 14:38:06 +0900

    [fix] reference

--- js/index.jsx
+++ js/index.jsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import ComponentBuilder from './Components/ComponentBuilder.jsx';
 import FIleInfoCompoent from './Components/FileInfoComponent.jsx';
 
